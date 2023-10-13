5e6a076bf2b9eee0ce05d7a3498fa085f219ae16
Scott Schumacker <scottschu97@gmail.com>
2023-10-13 11:07:20 -0400

    Added reactive table

--- Dashboard_App/app.R
+++ Dashboard_App/app.R
@@ -4,13 +4,15 @@
 # Loading libraries
 library(shiny)
 library(shinydashboard)
+library(ggplot2)
+library(dplyr)
 
 # UI
 ui <- dashboardPage(
   dashboardHeader(title = "Scott Schumacker"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("World Population", tabName = "worldPop", icon = icon("dashboard")),
+      menuItem("World Population", tabName = "worldPop", icon = icon("signal")),
       menuItem("Widgets", tabName = "widgets", icon = icon("th"))
     )
   ),
@@ -18,13 +20,25 @@ ui <- dashboardPage(
     tabItems(
       tabItem(tabName = "worldPop",
           box(title = "Controls",
-              selectInput(inputId = "countryName", "Country", choices = world_pop$country)
-              )    
+              selectInput(inputId = "countryName", "Country", choices = long$country)
+              ),
+          dataTableOutput("popTable")
     )
   ))
 )
 
 # Server
-server <- function(input, output) { }
+server <- function(input, output) {
+  
+  worldPlotData <- reactive({
+    long %>% 
+      filter(country == input$countryName)
+  })
+  
+  output$popTable <- renderDataTable({
+    worldPlotData()
+  })
+  
+}
 
 shinyApp(ui, server)
