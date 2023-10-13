e556751723d0ca1d7c300a364d05b58def78fa79
Scott Schumacker <scottschu97@gmail.com>
2023-10-13 10:33:41 -0400

    Added control panel

--- Dashboard_App/app.R
+++ Dashboard_App/app.R
@@ -8,8 +8,20 @@ library(shinydashboard)
 # UI
 ui <- dashboardPage(
   dashboardHeader(title = "Scott Schumacker"),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("World Population", tabName = "worldPop", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "worldPop",
+          box(title = "Controls",
+              selectInput(inputId = "countryName", "Country", choices = world_pop$country)
+              )    
+    )
+  ))
 )
 
 # Server
