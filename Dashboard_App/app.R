4e95b727d21c86692b51887275e0ddd4bef6f7f8
Scott Schumacker <scottschu97@gmail.com>
2023-12-06 18:45:25 -0500

    Added donut chart

--- Dashboard_App/app.R
+++ Dashboard_App/app.R
@@ -19,7 +19,9 @@ ui <- dashboardPage(
   # Sidebar
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Netflix Insights", tabName = "netflix", icon = icon("signal"))
+      menuItem("Netflix Insights", tabName = "netflix", icon = icon("signal")),
+      # Selector for countries UI
+      uiOutput("countryChoose")
     )
   ),
   # Body
@@ -35,11 +37,11 @@ ui <- dashboardPage(
         box(plotlyOutput("topTenOut"), width = "100%", title = 
               "Top Ten Countries by Number of Releases (2008-2021)"),
         
-        # Selector for countries UI
-        uiOutput("countryChoose"),
-        
         # Interactive point plot UI
-        box(plotlyOutput("releaseTime"), title = "Number of Release by Year")
+        box(plotlyOutput("releaseTime"), title = "Number of Release by Year"),
+        
+        # Interactive donut plot UI
+        box(plotOutput("donutUI"), title = "Type of release")
       )
     )
   )
@@ -51,7 +53,6 @@ server <- function(input, output) {
   netflixTitles <- netflix_titles3
   # Extracting only the year
   netflixTitles$dateadded <- substr(netflixTitles$date_added, 1, 4)
-  # Cleaning Netflix data set (add code)
   # Processing Netflix data for top ten visualization
   countryTable <- netflixTitles %>% 
     group_by(country) %>% 
@@ -74,31 +75,59 @@ server <- function(input, output) {
   
   # Creating plot output for top ten bar plot
   top10 <- ggplot(data = countryTable, aes(x = Country, y = Releases)) +
-    geom_bar(stat = "identity", fill = "red", color = "black", alpha = 0.7)
+    geom_bar(stat = "identity", fill = "red", color = "black", alpha = 0.8)
   top10
   top10Interactive <- ggplotly(top10)
   output$topTenOut <- renderPlotly(top10Interactive)
   
   # Creating DF for interactive point plot
-  testDF <- netflixTitles %>% 
+  pointDF <- netflixTitles %>% 
     select(type, country, date_added)
   
   # Tidying DF for interactive point plot
-  testDF$date_added <- as.Date(testDF$date_added)
-  testDF$date_added <- format(as.Date(testDF$date_added, 
+  pointDF$date_added <- as.Date(pointDF$date_added)
+  pointDF$date_added <- format(as.Date(pointDF$date_added, 
                                       format="%d/%m/%Y"), "%Y")
+  
+  # Creating DF for interactive donut chart
+  donutDF <- netflixTitles %>% 
+    select(type, country, rating)
+  
+  output$donutUI <- renderPlot({
+    donutDF2 <- donutDF %>% 
+      filter(country == input$countrySelect) %>% 
+      group_by(type) %>% 
+      summarise(total = n()) %>% 
+      na.omit()
+    
+    donutDF2$fraction <- donutDF2$total / sum(donutDF2$total)
+    donutDF2$ymax = cumsum(donutDF2$fraction)
+    donutDF2$ymin = c(0, head(donutDF2$ymax, n=-1))
+    donutDF2$labelPosition <- (donutDF2$ymax + donutDF2$ymin) / 2
+    donutDF2$label <- paste0(donutDF2$type, "\n frequency: ", round(donutDF2$fraction, 2))
+    donutP <- ggplot(donutDF2, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = type)) +
+      geom_rect() +
+      geom_label(x=3.5, aes(y=labelPosition, label = label), size=6) +
+      scale_fill_brewer(palette = "RdGy") +
+      coord_polar(theta = "y") +
+      xlim(c(2,4)) +
+      theme_void() +
+      theme(legend.position = "none") +
+      scale_fill_manual(values = c("red", "#f9f9f9"))
+    donutP
+  })
   
   output$releaseTime <- renderPlotly({
     
-    testDF2 <- testDF %>% 
+    pointDF2 <- pointDF %>% 
       filter(country == input$countrySelect) %>% 
       group_by(date_added) %>% 
       summarise(n = n()) %>% 
       na.omit()
     
     # Creating plot output for interactive point plot
-    p2 <- ggplot(testDF2, aes(x = date_added, y = n)) +
-      geom_point(size = 5, alpha = 0.6, color = "red") + xlab("Year") +
+    p2 <- ggplot(pointDF2, aes(x = date_added, y = n)) +
+      geom_point(size = 5, alpha = 0.8, color = "red") + xlab("Year") +
       ylab("Number of Releases") + geom_smooth(method = "lm")
     ggplotly(p2)
   })
